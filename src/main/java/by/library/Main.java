bd675e1ce6c8a88ef3395294104d31d48339ad6f
Kuleb9lka <101733691+Kuleb9lka@users.noreply.github.com>
2022-03-19 12:22:23 +0300

    Added user and lombok library. (#1)
    * Added user and lombok library.
    
    * Added user and lombok library.

--- src/main/java/by/library/Main.java
+++ src/main/java/by/library/Main.java
@@ -1,14 +1,26 @@
 package by.library;
 
+<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+=======
+import by.library.user.User;
+
+>>>>>>> d94ff46 (Added user and lombok library. (#1))
 public class Main {
 
     public static void main(String[] args) {
 
+<<<<<<< HEAD
         SpringApplication.run(Main.class, args);
 
+=======
+        User user = new User();
+        user.setAge(14);
+
+        System.out.println(user.getAge());
+>>>>>>> d94ff46 (Added user and lombok library. (#1))
     }
 }
