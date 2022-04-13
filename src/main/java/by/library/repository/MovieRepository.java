c3ee599d134cd7cba7e4137c3c1fd6e9180f3add
Kuleb9lka <101733691+Kuleb9lka@users.noreply.github.com>
2022-04-13 22:39:41 +0300

    Revert "Movie service and controller created. (#3)"
    This reverts commit 334fa87bf67a31a58cc6888a652fb57c5394316c.

--- src/main/java/by/library/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package by.library.repository;
-
-import by.library.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
--- src/main/java/by/library/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package by.library.repository;
-
-import by.library.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
